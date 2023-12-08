ac4676f22a65b8165de735b3bd1db73aac947ccf
Vadim <hawaiianwork@mail.ru>
2023-12-08 23:58:14 +0300

    updates

--- CarDealerClient/src/main/java/com/vadim/fxml_controllers/MainMenuUser.java
+++ CarDealerClient/src/main/java/com/vadim/fxml_controllers/MainMenuUser.java
@@ -1,4 +1,27 @@
 package com.vadim.fxml_controllers;
 
+import com.vadim.Car;
+import com.vadim.User;
+import com.vadim.CheckerInput;
+import com.vadim.RequestController;
+import com.vadim.Client;
+import com.vadim.tables.SuperMMID;
+import com.vadim.tables.Tables;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainMenuUser {
 }
