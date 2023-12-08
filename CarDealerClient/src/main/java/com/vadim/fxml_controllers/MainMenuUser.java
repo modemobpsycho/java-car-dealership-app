fc1129db08b135228ab41536d3a76e2a8d2325b1
Vadim <hawaiianwork@mail.ru>
2023-12-09 00:12:42 +0300

    updates

--- CarDealerClient/src/main/java/com/vadim/fxml_controllers/MainMenuUser.java
+++ CarDealerClient/src/main/java/com/vadim/fxml_controllers/MainMenuUser.java
@@ -24,4 +24,93 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainMenuUser {
+
+    @FXML
+    private TableView<Car> allCarsTable;
+    @FXML
+    private TableView<User> allUserTable;
+    @FXML
+    private TableView<User> allAdminsTable;
+    @FXML
+    private TableView<SuperMMID> userIncomingRequest;
+
+
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label loginLabel;
+
+
+    @FXML
+    private TableColumn<Car, String> makeColumn;
+    @FXML
+    private TableColumn<Car, String> modelColumn;
+    @FXML
+    private TableColumn<Car, String> bodyColumn;
+    @FXML
+    private TableColumn<Car, Integer> priceColumn;
+
+
+    @FXML
+    private TableColumn<User, Integer> idColumn;
+    @FXML
+    private TableColumn<User, String> nameColumn;
+    @FXML
+    private TableColumn<User, String> surnameColumn;
+    @FXML
+    private TableColumn<User, String> loginColumn;
+    @FXML
+    private TableColumn<User, String> mailColumn;
+
+
+    @FXML
+    private TableColumn<User, Integer> idColumn11;
+    @FXML
+    private TableColumn<User, String> nameColumn11;
+    @FXML
+    private TableColumn<User, String> surnameColumn11;
+    @FXML
+    private TableColumn<User, String> loginColumn11;
+    @FXML
+    private TableColumn<User, String> mailColumn11;
+
+
+    @FXML
+    private TableColumn<SuperMMID, String> mailColumn1;
+    @FXML
+    private TableColumn<SuperMMID, String> makeColumn1;
+    @FXML
+    private TableColumn<SuperMMID, String> idColumn1;
+    @FXML
+    private TableColumn<SuperMMID, String> makeColumn2;
+    @FXML
+    private TableColumn<SuperMMID, String> modelColumn2;
+    @FXML
+    private TableColumn<SuperMMID, Integer> idColumn2;
+
+
+    @FXML
+    private Label nameLabel;
+
+
+    @FXML
+    private TextField SearchTextField;
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField idTextField1;
+    @FXML
+    private TextField idTextField2;
+    @FXML
+    private TextField idTextField3;
+
+
+    private ObservableList<Car> CarsData = FXCollections.observableArrayList();
+    private ObservableList<User> Users = FXCollections.observableArrayList();
+    private ObservableList<User> Admins = FXCollections.observableArrayList();
+    private ObservableList<SuperMMID> IncomingRequests = FXCollections.observableArrayList();
+    private ObservableList<SuperMMID> AcceptedRequests = FXCollections.observableArrayList();
+
 }
